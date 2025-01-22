42f9208de76bb690efae4d98b9f8a9498e356a48
Matt Lacey <matt@mrlacey.co.uk>
2025-01-22 15:01:54 +0000

    initial Windows implementation of UseReducedMotion

--- src/Plugin.Maui.Accessibility/Feature.windows.cs
+++ src/Plugin.Maui.Accessibility/Feature.windows.cs
@@ -1,8 +1,43 @@
-﻿namespace Plugin.Maui.Accessibility;
+﻿using System.Runtime.InteropServices;
+
+namespace Plugin.Maui.Accessibility;
 
 partial class FeatureImplementation : IFeature
 {
 	// TODO Implement your Windows specific code
-	public PossiblyUnknownBool UseReducedMotion { get; }
+	public PossiblyUnknownBool UseReducedMotion
+	{
+		get
+		{
+			try
+			{
+				int isAnimationEnabled = 0;
+				NativeMethods.SystemParametersInfo(NativeMethods.SPI_GETCLIENTAREAANIMATION, 0, ref isAnimationEnabled, 0);
+
+				// Not that UseReducedMotion is the opposite of using animation (or having it enabled)
+				if (isAnimationEnabled == 0)
+				{
+					return PossiblyUnknownBool.True;
+				}
+				else
+				{
+					return PossiblyUnknownBool.False;
+				}
+			}
+			catch (Exception)
+			{
+				return PossiblyUnknownBool.Unknown;
+			}
+		}
+	}
+
 	public double TextScaleFactor { get; }
+}
+
+public static class NativeMethods
+{
+	public const int SPI_GETCLIENTAREAANIMATION = 0x1042;
+
+	[DllImport("user32.dll", EntryPoint = "SystemParametersInfo")]
+	internal static extern bool SystemParametersInfo(int uiAction, int uiParam, ref int pvParam, int fWinIni);
 }
