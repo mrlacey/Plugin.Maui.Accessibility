fe7ac889a1ee61f71169063ea29a53b1d62b371a
Matt Lacey <matt@mrlacey.co.uk>
2025-01-22 14:53:54 +0000

    initial Android implementation of UseReducedMotion

--- src/Plugin.Maui.Accessibility/Feature.android.cs
+++ src/Plugin.Maui.Accessibility/Feature.android.cs
@@ -1,8 +1,41 @@
-﻿namespace Plugin.Maui.Accessibility;
+﻿using Android.Provider;
+
+namespace Plugin.Maui.Accessibility;
 
 partial class FeatureImplementation : IFeature
 {
-	// TODO Implement your Android specific code
-	public PossiblyUnknownBool UseReducedMotion { get; }
+	public PossiblyUnknownBool UseReducedMotion
+	{
+		get
+		{
+			try
+			{
+				var resolver = global::Android.App.Application.Context?.ContentResolver;
+				if (resolver == null)
+				{
+					return PossiblyUnknownBool.Unknown;
+				}
+
+				// Values may not be set so default to "on" if not set
+				var duration = Settings.Global.GetFloat(resolver, Settings.Global.AnimatorDurationScale, 1.0f);
+				var transition = Settings.Global.GetFloat(resolver, Settings.Global.TransitionAnimationScale, 1.0f);
+
+				// Both will be zero if animation is disabled
+				if (duration < 0.01 && transition < 0.01)
+				{
+					return PossiblyUnknownBool.True;
+				}
+				else
+				{
+					return PossiblyUnknownBool.False;
+				}
+			}
+			catch (Exception)
+			{
+				return PossiblyUnknownBool.Unknown;
+			}
+		}
+	}
+
 	public double TextScaleFactor { get; }
 }
